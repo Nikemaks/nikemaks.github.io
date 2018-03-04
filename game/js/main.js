bd0ed3b284a873daf70cc1f4bf45cf79e068cbfe
maks <malyi.1992@mail.ru>
2018-03-04 21:12:06 +0200

    game.V1.1.01

--- game/js/main.js
+++ game/js/main.js
@@ -1,5 +1,7 @@
+window.onload = function ready(){
 for (var i = 0; i < 9; i++) {
 	document.getElementById('game').innerHTML+='<div class="block"></div>'
+}
 }
 var hod=0;
 document.getElementById('game').onclick=function(event) {
