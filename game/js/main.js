2dc557649294433fc0cf2e92203fb51fad85c115
maks <malyi.1992@mail.ru>
2018-03-04 21:55:22 +0200

    game.v.1.1
    обновил фон

--- game/js/main.js
+++ game/js/main.js
@@ -1,6 +1,6 @@
 window.onload = function ready(){
 for (var i = 0; i < 9; i++) {
-	document.getElementById('game').innerHTML+='<div class="block"></div>'
+	document.getElementById('game').innerHTML+='<div class="block"></div>';
 }
 }
 var hod=0;
