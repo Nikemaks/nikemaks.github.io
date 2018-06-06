7388e47ef3fef0d13c3491e093542e19f1e9c4fd
maks <malyi.1992@mail.ru>
2018-06-07 01:09:07 +0300

    Розрахунок калорій

---
+++ Розрахунок Калорій/js.js
@@ -0,0 +1,7 @@
+$(function() {
+	alert('hello');
+	// $(".products").change(function() {
+	// alert( "Handler for .change() called." );
+	// var sum = $('#products').val();
+	// $('#kkal').val(sum);
+});
---
+++ Розрахунок Калорій/script.js
@@ -0,0 +1,211 @@
+$(".result_btn").click(function() {
+		var num = $("#result").val();
+			var num2 = $("#result1").val();
+	  var sumand = parseInt(num) + parseInt(num2);
+
+	
+	 $("#resultand").val(sumand);
+});
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products').val();
+	$('#kkal').val(sum);
+	var kkal = $("#kkal").val();
+	var gram = $('#gram').val();
+	var res = (gram*kkal)/100;
+	$('#result').val(res);
+  });
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products2').val();
+	$('#kkal2').val(sum);
+	var kkal = $("#kkal2").val();
+	var gram = $('#gram2').val();
+	var res = (gram*kkal)/100;
+	$('#result2').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products3').val();
+	$('#kkal3').val(sum);
+	var kkal = $("#kkal3").val();
+	var gram = $('#gram3').val();
+	var res = (gram*kkal)/100;
+	$('#result3').val(res);
+  });
+	
+	});
+
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products4').val();
+	$('#kkal4').val(sum);
+	var kkal = $("#kkal4").val();
+	var gram = $('#gram4').val();
+	var res = (gram*kkal)/100;
+	$('#result4').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products5').val();
+	$('#kkal5').val(sum);
+	var kkal = $("#kkal5").val();
+	var gram = $('#gram5').val();
+	var res = (gram*kkal)/100;
+	$('#result5').val(res);
+  });
+	
+	});
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products6').val();
+	$('#kkal6').val(sum);
+	var kkal = $("#kkal6").val();
+	var gram = $('#gram6').val();
+	var res = (gram*kkal)/100;
+	$('#result6').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products7').val();
+	$('#kkal7').val(sum);
+	var kkal = $("#kkal7").val();
+	var gram = $('#gram7').val();
+	var res = (gram*kkal)/100;
+	$('#result7').val(res);
+  });
+	
+	});
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products8').val();
+	$('#kkal8').val(sum);
+	var kkal = $("#kkal8").val();
+	var gram = $('#gram8').val();
+	var res = (gram*kkal)/100;
+	$('#result8').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products9').val();
+	$('#kkal9').val(sum);
+	var kkal = $("#kkal9").val();
+	var gram = $('#gram9').val();
+	var res = (gram*kkal)/100;
+	$('#result9').val(res);
+  });
+	
+	});
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products10').val();
+	$('#kkal10').val(sum);
+	var kkal = $("#kkal10").val();
+	var gram = $('#gram10').val();
+	var res = (gram*kkal)/100;
+	$('#result10').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products11').val();
+	$('#kkal11').val(sum);
+	var kkal = $("#kkal11").val();
+	var gram = $('#gram11').val();
+	var res = (gram*kkal)/100;
+	$('#result11').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products12').val();
+	$('#kkal12').val(sum);
+	var kkal = $("#kkal12").val();
+	var gram = $('#gram12').val();
+	var res = (gram*kkal)/100;
+	$('#result12').val(res);
+  });
+	
+	});
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products13').val();
+	$('#kkal13').val(sum);
+	var kkal = $("#kkal13").val();
+	var gram = $('#gram13').val();
+	var res = (gram*kkal)/100;
+	$('#result13').val(res);
+  });
+	
+	});
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products14').val();
+	$('#kkal14').val(sum);
+	var kkal = $("#kkal14").val();
+	var gram = $('#gram14').val();
+	var res = (gram*kkal)/100;
+	$('#result14').val(res);
+  });
+	
+	});
+
+
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products15').val();
+	$('#kkal15').val(sum);
+	var kkal = $("#kkal15").val();
+	var gram = $('#gram15').val();
+	var res = (gram*kkal)/100;
+	$('#result15').val(res);
+  });
+	
+	});
+
+$(function() {
+	$(".products").change(function() {
+	var sum = $('#products16').val();
+	$('#kkal16').val(sum);
+	var kkal = $("#kkal16").val();
+	var gram = $('#gram16').val();
+	var res = (gram*kkal)/100;
+	$('#result16').val(res);
+  });
+	
+	});
