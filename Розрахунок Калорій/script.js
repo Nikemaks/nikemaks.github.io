dfba40de98b9cb2ef6c41058d710389b73fb54b6
maks <malyi.1992@mail.ru>
2018-06-17 13:33:26 +0300

    Розраз кал кнопка

--- Розрахунок Калорій/script.js
+++ Розрахунок Калорій/script.js
@@ -1,9 +1,73 @@
 $(".result_btn").click(function() {
-		var num = $("#result").val();
-			var num2 = $("#result1").val();
-	  var sumand = parseInt(num) + parseInt(num2);
+	var kkal = $("#kkal").val();
+	var gram = $('#gram').val();
+	var res = (gram*kkal)/100;
 
+	var kkal1 = $("#kkal2").val();
+	var gram1 = $('#gram2').val();
+	var res1 = (gram1*kkal1)/100;
+
+	var kkal2 = $("#kkal2").val();
+	var gram2 = $('#gram2').val();
+	var res2 = (gram2*kkal2)/100;
+
+	var kkal3 = $("#kkal3").val();
+	var gram3 = $('#gram3').val();
+	var res3 = (gram3*kkal3)/100;
+
+	var kkal4 = $("#kkal4").val();
+	var gram4 = $('#gram4').val();
+	var res4 = (gram4*kkal4)/100;
+
+	var kkal5 = $("#kkal5").val();
+	var gram5 = $('#gram5').val();
+	var res5 = (gram5*kkal5)/100;
+
+	var kkal6 = $("#kkal6").val();
+	var gram6 = $('#gram6').val();
+	var res6 = (gram6*kkal6)/100;
+
+	var kkal7 = $("#kkal7").val();
+	var gram7 = $('#gram7').val();
+	var res7 = (gram4*kkal7)/100;
+
+	var kkal8 = $("#kkal8").val();
+	var gram8 = $('#gram8').val();
+	var res8 = (gram8*kkal8)/100;
+
+	var kkal9 = $("#kkal9").val();
+	var gram9 = $('#gram9').val();
+	var res9 = (gram9 * kkal9)/100;
+
+	var kkal10 = $("#kkal10").val();
+	var gram10 = $('#gram10').val();
+	var res10 = (gram10*kkal10)/100;
+
+	var kkal11 = $("#kkal11").val();
+	var gram11 = $('#gram11').val();
+	var res11 = (gram4*kkal11)/100;
+
+	var kkal12 = $("#kkal12").val();
+	var gram12 = $('#gram12').val();
+	var res12 = (gram12*kkal12)/100;
+
+	var kkal13 = $("#kkal13").val();
+	var gram13 = $('#gram13').val();
+	var res13 = (gram13*kkal13)/100;
+
+	var kkal14 = $("#kkal14").val();
+	var gram14 = $('#gram14').val();
+	var res14 = (gram14*kkal14)/100;
+
+		var kkal15 = $("#kkal15").val();
+	var gram15 = $('#gram15').val();
+	var res15 = (gram4*kkal15)/100;
+
+	var kkal16 = $("#kkal16").val();
+	var gram16 = $('#gram16').val();
+	var res16 = (gram16*kkal16)/100;
 	
+	var sumand = res + res1 + res2 + res3 + res4 + res5 + res6 + res7 + res8 + res9 + res10 + res11 + res12 + res13 + res14 + res15 + res16;
 	 $("#resultand").val(sumand);
 });
 
