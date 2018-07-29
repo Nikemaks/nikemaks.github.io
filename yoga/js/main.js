c6858376259cb7bc33568128b1264295421152ac
maks <malyi.1992@mail.ru>
2018-07-29 18:16:38 +0300

    module

---
+++ yoga/js/main.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', function(){
+    let tab = require('../js/tab.js');
+    let timer = require('../js/timer.js');  
+    let slider = require('../js/slider.js');  
+    let calc = require('../js/calc.js');  
+    let modal = require('../js/modal.js');  
+
+
+    tab();
+    timer();
+    slider();
+    calc();
+    modal();
+
+
+});
