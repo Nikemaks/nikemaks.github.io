be2b3fe6542b976e09663c4a066a39b9585b9caa
maks <malyi.1992@mail.ru>
2018-08-02 20:33:55 +0300

    slider 1

--- diplom/js/script.js
+++ diplom/js/script.js
@@ -25,15 +25,12 @@ document.addEventListener("DOMContentLoaded", function() {
 			let male = document.getElementById('male'),
 							female = document.getElementById('female'),
 							radioBtn = document.getElementsByClassName('radio')[0],
-							person_skin = document.getElementsByClassName('person-skin')[0],
+							person_skin = document.getElementById('person-skin'),
 							person_clothes = document.getElementsByClassName('person-clothes')[0],
 							person_hair = document.getElementsByClassName('person-hair')[0],
 
 
-							skin_color_1 = document.querySelector('skin-color-1'),
-							skin_color_2 = document.querySelector('skin-color-2'),
-							skin_color_3 = document.querySelector('skin-color-3'),
-
+					
 
 							hair_style_1 = document.querySelector('hair-style-1'),
 							hair_style_2 = document.querySelector('hair-style-2'),
@@ -56,12 +53,96 @@ document.addEventListener("DOMContentLoaded", function() {
 			person_clothes.style.background = 'url(../img/clothes/construct/clothes-4.png) center no-repeat';
 			person_hair.style.background = 'url(../img/hair/construct/hair-4.png) center no-repeat';
 
+
 });
 
 
+			// цвет кожи слайдер
+		let		prev = document.querySelector('.prev'),
+							next = document.querySelector('.next');
+
+			let	slideIndex = 1,
+						 skin_color_1 = document.getElementsByClassName('skin-color-1')[0],
+							skin_color_2 = document.getElementsByClassName('skin-color-2')[0],
+							skin_color_3 = document.getElementsByClassName('skin-color-3')[0],
+							skin_color = document.getElementsByClassName('skin-color');
+	
+			function showSlide(n){
+				if (n > skin_color.length) {
+						slideIndex = 1;
+				};
+
+				if (n < 1) {
+					slideIndex = skin_color.length;
+
+				};
+				for (let i = 0; i < skin_color.length; i++) {
+					skin_color[i].style.display = 'none';
+				};
+
+				skin_color[slideIndex - 1].style.display = 'block';
+
+			};
+
+			function plusSlides(n) {
+				showSlide(slideIndex += n);
+			};
+
+			next.addEventListener('click', function(){
+				 plusSlides(1);
+				 colorSkin(slideIndex);
+
+			});
+
+			prev.addEventListener('click', function(){
+					plusSlides(-1);
+					colorSkin(slideIndex);
+			});
+	// изменения на самой модельке
+	function colorSkin(a){
+
+			if(a == 1){
+						 person_skin.classList.remove('person-skin');
+						 person_skin.classList.add('person-skin-2');
+						 person_skin.classList.remove('person-skin-3')
+
+			};
+
+			if (a == 2){
+							person_skin.classList.remove('person-skin-3');
+						 person_skin.classList.add('person-skin-2');		
+						 person_skin.classList.remove('person-skin')
+			};
+
+				if (a == 3){
+							person_skin.classList.remove('person-skin-2');
+						 person_skin.classList.add('person-skin-3');
+						 person_skin.classList.remove('person-skin')
+			};
+
+			
+	};
 	
 
+// Изменения прически
 
+let indexHair = 1,
+				next_hair = document.getElementsByClassName('next-hair')[0],
+				prev_hair = document.getElementsByClassName('prev-hair')[0];
+				hair_style = document.getElementsByClassName('hair-style');
+
+next_hair.addEventListener('click', function(){
+
+});
+
+function sliderHair(b){
+	if (b > hair_style.length) {
+			indexHair = 1;
+	};
+	if (b < 1){
+			indexHair = hair_style.length ;
+	};
+};
 
 	
 });
