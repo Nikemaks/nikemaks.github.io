799066ce9eb75c04e61787fccd22de2494a51ec1
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-01-29 10:43:46 +0200

    hw_10

---
+++ hw/script.js
@@ -0,0 +1,36 @@
+
+var studentList = [ student_1 = {
+  name: 'Vasay',
+  dataStart: 2001,
+  dataEnd: 2006
+},  student_2 = {
+  name: 'Petya',
+  dataStart: 2003,
+  dataEnd: 2008
+},student_3 = {
+  name: 'Andrey',
+  dataStart: 2004,
+  dataEnd: 2007
+}, student_4 = {
+  name: 'Kiril',
+  dataStart: 2005,
+  dataEnd: 2007
+},student_5 = {
+  name: 'Kiril2',
+  dataStart: 2002,
+  dataEnd: 2004
+}];
+getListStudent(2005, 2008);
+
+function getListStudent(dataStart, dataEnd) {
+   for (var i = 0; i < studentList.length; i++) {
+    if (studentList[i].dataStart >= dataStart && studentList[i].dataEnd <= dataEnd || studentList[i].dataStart <= dataStart && studentList[i].dataEnd <= dataEnd && studentList[i].dataEnd >= dataStart ) {
+     console.log(studentList[i]);
+    }
+  }
+}
+//2 
+//  getMaxStudent(2001, 2005);
+// function getMaxStudent(dataStart, dataEnd) {
+
+// } 
