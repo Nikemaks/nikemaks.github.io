8a7ce2985be9cb3fa2017f7d5eab87bfcefe6313
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-01-29 11:42:25 +0200

    2

--- hw/script.js
+++ hw/script.js
@@ -20,20 +20,48 @@ var studentList = [ student_1 = {
   dataStart: 2002,
   dataEnd: 2004
 }];
-getListStudent(2005, 2008);
 
-function getListStudent(dataStart, dataEnd) {
-  let arrayStudent = [];
-   for (var i = 0; i < studentList.length; i++) {
+// getListStudent(2005, 2008);
+
+// function getListStudent(dataStart, dataEnd){
+//   let arrayStudent = [];
+//    for (var i = 0; i < studentList.length; i++) {
+//     if (studentList[i].dataStart >= dataStart && studentList[i].dataEnd <= dataEnd || studentList[i].dataStart <= dataStart && studentList[i].dataEnd <= dataEnd && studentList[i].dataEnd >= dataStart ) {
+//      console.log(studentList[i]);
+//      arrayStudent.push(studentList[i]);
+//     }
+//   }
+//   return arrayStudent;
+// }
+//2 
+ getMaxStudent(2002, 2008);
+function getMaxStudent(dataStart, dataEnd) {
+  let arrayStudent = [],
+      counter = [],
+      years = [],
+      count = 0,
+      arrayRepeat = [];
+  for (var i = 0; i < studentList.length; i++) {
     if (studentList[i].dataStart >= dataStart && studentList[i].dataEnd <= dataEnd || studentList[i].dataStart <= dataStart && studentList[i].dataEnd <= dataEnd && studentList[i].dataEnd >= dataStart ) {
      console.log(studentList[i]);
      arrayStudent.push(studentList[i]);
     }
   }
-  return arrayStudent;
-}
-//2 
-//  getMaxStudent(2001, 2005);
-// function getMaxStudent(dataStart, dataEnd) {
 
-// } 
+  for (var k = dataStart; k < dataEnd; k++) {
+    for (var j = 0; j < arrayStudent.length; j++) {
+      if( k == arrayStudent[j].dataStart || k == arrayStudent[j].dataEnd ){
+         years.push(k);
+      }
+    };
+  }
+  
+  for (var m = 0; m < years.length; m++) {
+    for (var h = 0; h < years.length; h++) {
+     if (years[m] == years[h]) {
+         
+     }; 
+    };
+  };
+
+} 
