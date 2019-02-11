bbc836b63e5120655e4c18a0ddbc9dc8f3f17226
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-02-11 22:27:34 +0200

    new
    new

---
+++ hilell/js/script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+let sidebar = document.getElementById('left-sidebar'),
+    content = document.getElementById('main-contant'),
+    buton_menu = document.getElementById('btn-menu');
+
+buton_menu.onclick = function(){
+	if (content.classList.contains('sadebar-active')) {
+       content.classList.remove("sadebar-active");
+       sidebar.classList.remove("sadebar-left");
+	}else{
+		content.classList.add("sadebar-active");
+		sidebar.classList.add("sadebar-left");
+	}
+};
+
+}
