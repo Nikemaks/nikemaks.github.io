c1551c3a412ea91e461567594b32a4b1f368c653
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-02-27 19:32:15 +0200

    hw_19_2
    дата и часы

--- hilell/js/script.js
+++ hilell/js/script.js
@@ -12,5 +12,58 @@ buton_menu.onclick = function(){
 		sidebar.classList.add("sadebar-left");
 	}
 };
+function showTime(hour, minuts, seconds) {
+let  timeHours = document.getElementsByClassName('time-hours')[0],
+	 timeMinutes = document.getElementsByClassName('time-minutes')[0],
+	 timeSeconds = document.getElementsByClassName('time-seconds')[0];
+
+	if (+timeHours.innerHTML !== hour ) {
+		timeHours.innerHTML = hour;
+	}
+	if (+timeMinutes.innerHTML !== minuts ) {
+		timeMinutes.innerHTML = minuts;
+	}	
+	if (+timeSeconds.innerHTML !== seconds ) {
+		timeSeconds.innerHTML = seconds;
+	}
+
+};
+let checkTime = () => {
+	let time = new Date(),
+		hour = time.getHours(),
+		minuts = time.getMinutes(),
+		seconds = time.getSeconds();
+
+	if(minuts.toString().length == 1){
+		minuts = '0' + minuts;
+	}  else {
+			minuts;
+	}
+	if(seconds.toString().length == 1){
+	 	seconds = '0' + seconds;
+	} else{
+		 seconds;
+	}
+
+	showTime(hour, minuts, seconds)
+} 
+setInterval(checkTime, 1000);
+
+}
+showDate();
+(function generateDate(){
+	let date = new Date(),
+		number = date.getDate();
+		mounth = date.getMonth(),
+		arrayMonth = ["Января","Февраля","Марта","Апреля","Майя","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"],
+		mounthText = arrayMonth[mounth];
+showDate(number,mounthText);	
+})();
+function showDate(number,mounthText){
+	let dateNumber = document.querySelector('.date-number'),
+		dateMonth = document.querySelector('.date-month');
+		dateNumber.innerHTML = number;
+		dateMonth.innerHTML = mounthText;
+
 
 }
