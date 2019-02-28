8038620f2de7dae71d49d2b9c8df895de0481b9c
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-02-28 22:48:10 +0200

    HW_19-4
    dz

--- hilell/js/script.js
+++ hilell/js/script.js
@@ -1,69 +1,95 @@
 window.onload = function(){
-let sidebar = document.getElementById('left-sidebar'),
-    content = document.getElementById('main-contant'),
-    buton_menu = document.getElementById('btn-menu');
+	let sidebar = document.getElementById('left-sidebar'),
+	    content = document.getElementById('main-contant'),
+	    buton_menu = document.getElementById('btn-menu');
 
-buton_menu.onclick = function(){
-	if (content.classList.contains('sadebar-active')) {
-       content.classList.remove("sadebar-active");
-       sidebar.classList.remove("sadebar-left");
-	}else{
-		content.classList.add("sadebar-active");
-		sidebar.classList.add("sadebar-left");
-	}
-};
-function showTime(hour, minuts, seconds) {
-let  timeHours = document.getElementsByClassName('time-hours')[0],
-	 timeMinutes = document.getElementsByClassName('time-minutes')[0],
-	 timeSeconds = document.getElementsByClassName('time-seconds')[0];
+	buton_menu.onclick = function(){
+		if (content.classList.contains('sadebar-active')) {
+	       content.classList.remove("sadebar-active");
+	       sidebar.classList.remove("sadebar-left");
+		}else{
+			content.classList.add("sadebar-active");
+			sidebar.classList.add("sadebar-left");
+		}
+	};
+	function showTime(hour, minuts, seconds) {
+	let  timeHours = document.getElementsByClassName('time-hours')[0],
+		 timeMinutes = document.getElementsByClassName('time-minutes')[0],
+		 timeSeconds = document.getElementsByClassName('time-seconds')[0];
 
-	if (+timeHours.innerHTML !== hour ) {
-		timeHours.innerHTML = hour;
+		if (+timeHours.innerHTML !== hour ) {
+			timeHours.innerHTML = hour;
+		}
+		if (+timeMinutes.innerHTML !== minuts ) {
+			timeMinutes.innerHTML = minuts;
+		}	
+		if (+timeSeconds.innerHTML !== seconds ) {
+			timeSeconds.innerHTML = seconds;
+		}
+	};
+	let checkTime = () => {
+		let time = new Date(),
+			hour = time.getHours(),
+			minuts = time.getMinutes(),
+			seconds = time.getSeconds();
+
+		if(minuts.toString().length == 1){
+			minuts = '0' + minuts;
+		}  else {
+				minuts;
+		}
+		if(seconds.toString().length == 1){
+		 	seconds = '0' + seconds;
+		} else{
+			 seconds;
+		}
+
+		showTime(hour, minuts, seconds)
+	} 
+	setInterval(checkTime, 1000);
+
+	showDate();
+	(function generateDate(){
+		let date = new Date(),
+			number = date.getDate();
+			mounth = date.getMonth(),
+			arrayMonth = ["Января","Февраля","Марта","Апреля","Майя","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"],
+			mounthText = arrayMonth[mounth];
+	showDate(number,mounthText);	
+	})();
+	function showDate(number,mounthText){
+		let dateNumber = document.querySelector('.date-number'),
+			dateMonth = document.querySelector('.date-month');
+			dateNumber.innerHTML = number;
+			dateMonth.innerHTML = mounthText;
 	}
-	if (+timeMinutes.innerHTML !== minuts ) {
-		timeMinutes.innerHTML = minuts;
+
+	window.onresize = function(){
+		timer();
+	};
+	function timer(){
+		let date = new Date(),
+			seconds = date.getSeconds();
+
+		let timer = setTimeout(function(){
+			if (seconds == seconds) {
+				paintScreen();
+				clearTimeout(timer);
+			}
+		}, 2000);
+	};
+	function paintScreen(){
+		let widthScreen = document.body.clientWidth,
+			heightScreen = document.body.clientHeight,
+			height = document.querySelector('.height'),
+			width = document.querySelector('.width');
+
+		height.innerHTML = heightScreen;
+		width.innerHTML = widthScreen;
 	}	
-	if (+timeSeconds.innerHTML !== seconds ) {
-		timeSeconds.innerHTML = seconds;
+
+	function timespend(){
+
 	}
-
-};
-let checkTime = () => {
-	let time = new Date(),
-		hour = time.getHours(),
-		minuts = time.getMinutes(),
-		seconds = time.getSeconds();
-
-	if(minuts.toString().length == 1){
-		minuts = '0' + minuts;
-	}  else {
-			minuts;
-	}
-	if(seconds.toString().length == 1){
-	 	seconds = '0' + seconds;
-	} else{
-		 seconds;
-	}
-
-	showTime(hour, minuts, seconds)
-} 
-setInterval(checkTime, 1000);
-
-}
-showDate();
-(function generateDate(){
-	let date = new Date(),
-		number = date.getDate();
-		mounth = date.getMonth(),
-		arrayMonth = ["Января","Февраля","Марта","Апреля","Майя","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"],
-		mounthText = arrayMonth[mounth];
-showDate(number,mounthText);	
-})();
-function showDate(number,mounthText){
-	let dateNumber = document.querySelector('.date-number'),
-		dateMonth = document.querySelector('.date-month');
-		dateNumber.innerHTML = number;
-		dateMonth.innerHTML = mounthText;
-
 
 }
