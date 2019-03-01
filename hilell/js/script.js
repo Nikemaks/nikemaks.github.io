663fdabf399a6cf286e16eb563b12774e32803cb
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-03-01 16:20:15 +0200

    HW_19_2
    создания таблицы по кнопке

--- hilell/js/script.js
+++ hilell/js/script.js
@@ -112,4 +112,32 @@ window.onload = function(){
 		}
 
 	});
+	//table
+	let button = document.querySelector(".create-table"),
+		paintTab = document.getElementById("picture-table");
+	button.onclick = function(){
+		let columnTable = +document.querySelector(".column").value,
+			rowTable = +document.querySelector(".row").value,
+			pictureTable = document.querySelector(".picture-table");
+
+		if (pictureTable.childNodes.length == 1 && rowTable < 10 && columnTable < 10) {
+			createTable(rowTable, columnTable);
+		} else {
+			paintTab.removeChild(paintTab.children[0]);
+		}
+	}
+
+	function createTable(rowTable, columnTable) {
+		let	tagTable = document.createElement("table"),
+			trTable = document.createElement("tr"),
+			tdTable = document.createElement("td");
+
+			paintTab.appendChild(tagTable);
+			for (let i = 0; i < rowTable ; i++) {
+				paintTab.childNodes[1].appendChild(trTable.cloneNode(true));
+				for (let j = 0; j < columnTable; j++) {
+					paintTab.childNodes[1].children[i].appendChild(tdTable.cloneNode(true));
+				}
+			}	 
+	}
 }
