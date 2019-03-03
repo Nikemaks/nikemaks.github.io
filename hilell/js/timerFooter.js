6c637d8a0db597eed7398ba604c368018d3c0dcc
Maxim Malyi <malyi.1992@mail.ru>
2019-03-03 17:52:31 +0200

    fix timeer

--- hilell/js/timerFooter.js
+++ hilell/js/timerFooter.js
@@ -1,24 +1,26 @@
 var sec = 0;
-	function timespend(val){
-		return (val > 9) ? val : "0" + val;
-	}
-	function goTimer() {
-		document.querySelector('.footer-timer_seconds').innerHTML = timespend(++sec%60);
-		document.querySelector('.footer-timer_minutes').innerHTML = timespend(parseInt(sec/60,10));
-		document.querySelector('.footer-timer_hours').innerHTML = timespend(parseInt(sec/3600,10));
-	}
-	let footerTimer = setInterval(goTimer, 1000);
-	
-	document.getElementById('footer-timer').addEventListener("mouseover", stopTimer);
-	document.getElementById('footer-timer').addEventListener("mouseleave", function(){
-		 footerTimer = setInterval(goTimer, 1000);
-	});
-	 function stopTimer() {
-		clearInterval(footerTimer);
-	};
-	document.addEventListener("keydown", function(event){
-		if (event.which == 27) {
-			sec = 0;
-		}
 
-	});
+function timespend(val){
+	return (val > 9) ? val : "0" + val;
+}
+function goTimer(seconds, minutes, hours) {
+	document.querySelector('.footer-timer_seconds').innerHTML = seconds || timespend(++sec%60);
+	document.querySelector('.footer-timer_minutes').innerHTML = minutes || timespend(parseInt(sec/60,10));
+	document.querySelector('.footer-timer_hours').innerHTML = hours || timespend(parseInt(sec/3600,10));
+}
+let footerTimer = setInterval(goTimer, 1000);
+
+document.getElementById('footer-timer').addEventListener("mouseover", stopTimer);
+document.getElementById('footer-timer').addEventListener("mouseleave", function(){
+	 footerTimer = setInterval(goTimer, 1000);
+});
+ function stopTimer() {
+	clearInterval(footerTimer);
+};
+document.addEventListener("keydown", function(event){
+	if (event.which == 27) {
+		sec = 0;
+		goTimer('00', '00', '00');
+	}
+
+});
