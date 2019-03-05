3723b96e2dd7296d8774414d73e3d597b007ef47
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-03-06 01:11:28 +0200

    slider done

--- hilell/js/Slider.js
+++ hilell/js/Slider.js
@@ -1,38 +1,48 @@
-function SliderMax(element){
-this.el = document.querySelector(element);
-this.init();
-//this.slide();
-}
 
-SliderMax.prototype = {
-	init: function () {
-		this.wrapper =  this.el.querySelector('.slider-wrapp');
-		this.link = this.el.querySelectorAll('.slider-dots ul li');
-		this.prevBtn = this.el.querySelector('.prev');
-		this.nextBtn = this.el.querySelector('.next');	
-	},
-	navigation: function () {
-
-	},
-
-	slide: function(element){
-		var index = parseInt(element.getAttribute("data-slide"),10) + 1;
-		var currentSlide = this.el.querySelector(".slider-item:nth-child("+ index +")");
-		this.prevBtn.addEventListener('click', function (e) {
-			
-		});	
-		this.nextBtn.addEventListener('click', function (e) {
-			wrapper.style.left = '-' + currentSlide.offsetLeft +'px';
-			console.log(currentSlide.offsetLeft);
-
-		});	
-
-	},
-	setCurrentLink: function () {
-
-	},
-	autoplay: function () {
-		
+function ready(){
+	function SliderMax(element, speed){
+	this.el = document.querySelector(element);
+	this.speed = speed;
+	this.init(speed);
+	
 	}
-};
-var slider1 = new SliderMax("#slider");
+
+	SliderMax.prototype = {
+		init: function (speed) {
+			this.wrapper =  this.el.querySelector('.slider-wrapp');
+			this.link = this.el.querySelectorAll('.slider-dots ul li');
+			this.prevBtn = this.el.querySelector('.prev');
+			this.nextBtn = this.el.querySelector('.next');
+			this.slideAll = this.el.querySelectorAll('.slider-item');
+			this.slide = this.el.querySelector('.slider-item');
+			this.speed = speed;
+			this.autoplay(this.wrapper,this.slide,this.slideAll, this.speed);	
+			console.log(this.slide.childNodes);
+		},
+		navigation: function () {
+
+		},
+
+		slide: function(element){	
+
+		},
+		setCurrentLink: function () {
+
+		},
+		autoplay: function (wrapper,slide,slideAll, speed) {
+			let offsetSlid = slide.offsetWidth; 
+			let autoplayTimer = setInterval(function(){
+				if ((slideAll.length * slide.offsetWidth) == offsetSlid) {
+					wrapper.style.left = 0;
+					offsetSlid = 0;
+				} else {
+					wrapper.style.left = '-' + offsetSlid +'px';
+					offsetSlid += slide.offsetWidth;
+				}
+			}, speed);	
+			
+		}
+	};
+var slider1 = new SliderMax("#slider", 2500);
+}
+document.addEventListener("DOMContentLoaded", ready);
