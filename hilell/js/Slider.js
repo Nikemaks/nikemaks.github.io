c18e5e646533c261c643ecf816aaeffc8f6e70c0
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-03-11 20:05:15 +0200

    new slider

--- hilell/js/Slider.js
+++ hilell/js/Slider.js
@@ -14,21 +14,46 @@ function ready(){
 			this.nextBtn = this.el.querySelector('.next');
 			this.slideAll = this.el.querySelectorAll('.slider-item');
 			this.slide = this.el.querySelector('.slider-item');
-			this.speed = speed;
-			this.autoplay(this.wrapper,this.slide,this.slideAll, this.speed);	
+			this.speed = speed || 2000;
+			this.autoplay(this.wrapper,this.slide,this.slideAll, this.speed);
+			this.navigation(this.prevBtn,this.nextBtn,this.wrapper,this.slide,this.slideAll);
 		},
-		navigation: function () {
+		navigation: function (prevBtn, nextBtn,wrapper,slide,slideAll) {
+			let offsetSlid = slide.offsetWidth;
+			prevBtn.addEventListener('click', function() {
+				if (offsetSlid == 0) {
+					offsetSlid =  (slideAll.length * slide.offsetWidth) - slide.offsetWidth;
+					wrapper.style.left = '-' + offsetSlid  +'px';
+				} else{
+					offsetSlid -=  slide.offsetWidth;
+					wrapper.style.left = '-' + offsetSlid  +'px';
+				}
+			
+			});
+			nextBtn.addEventListener('click', function() {
+				if ((slideAll.length * slide.offsetWidth) == offsetSlid) {
+					wrapper.style.left = 0;
+					offsetSlid = 0;
+				} else {
+					wrapper.style.left = '-' + offsetSlid +'px';
+					offsetSlid += slide.offsetWidth;
+				}
 
+			});
 		},
-		slide: function(element){	
-
+		stopSlide: function(autoplayTimer,wrapper,slide,slideAll,speed){	
+			this.wrapper.addEventListener('mouseover', function(){
+				clearInterval(autoplayTimer);
+			});
+			this.wrapper.addEventListener('mouseout', function(){
+				
+			});
 		},
 		setCurrentLink: function () {
 
 		},
-		autoplay: function (wrapper,slide,slideAll, speed) {
+		autoplay: function (wrapper,slide,slideAll,speed) {
 			let offsetSlid = slide.offsetWidth; 
-
 			let autoplayTimer = setInterval(function(){
 				if ((slideAll.length * slide.offsetWidth) == offsetSlid) {
 					wrapper.style.left = 0;
@@ -38,10 +63,10 @@ function ready(){
 					offsetSlid += slide.offsetWidth;
 				}
 			}, speed);	
-			
+			this.stopSlide(autoplayTimer,wrapper,slide,slideAll,speed);
 		}
 	};
-var slider1 = new SliderMax("#slider", 2500);
-var slider1 = new SliderMax("#slider2", 3500);
+var slider1 = new SliderMax("#slider", 2000);
+// var slider1 = new SliderMax("#slider2", 3500);
 }
 document.addEventListener("DOMContentLoaded", ready);
