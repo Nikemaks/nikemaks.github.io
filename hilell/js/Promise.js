3da41ce24ea97760cc10bd20f219845e8b493f55
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-03-27 14:15:33 +0200

    update

--- hilell/js/Promise.js
+++ hilell/js/Promise.js
@@ -1,6 +1,6 @@
 var promise = new Promise(function(resolve, reject) {
   var progressBar = document.getElementById('progressBar');
-  let buttonOneProgress = document.querySelector('.button-one').addEventListener('click', function  () {
+  let buttonAllProgress = document.querySelector('.button-two').addEventListener('click', function  () {
   	updateProgressBar();
   })
   function updateProgressBar() {
@@ -24,13 +24,39 @@ promise
     }
   );
 
+let elemArticle = document.querySelectorAll('.section-promise__content-element');
 function  animationArticle(){
-	let elemArticle = document.querySelectorAll('.section-promise__content-element');
 	for (var i = 0; i < elemArticle.length; i++) {
 		console.log(elemArticle[i].childNodes);
 		elemArticle[i].childNodes[1].classList.add('opacityZeroAnimate');
-		//elemArticle[i].childNodes[3].classList.add('opacityZeroAnimate');
 	}
+  setTimeout(animationArticleText, 1500);
 }
 
+function  animationArticleText(){
+  for (var i = 0; i < elemArticle.length; i++) {
+    elemArticle[i].childNodes[3].childNodes[3].classList.add('opacityZeroAnimate');
+  }
+  setTimeout(animationArticleTitle, 1500);
+}
+function  animationArticleTitle(){
+ for (var i = 0; i < elemArticle.length; i++) {
+    elemArticle[i].childNodes[3].childNodes[1].classList.add('opacityZeroAnimate');
+  }
+  setTimeout(animationArticleBlock, 1500)
 
+}
+function animationArticleBlock() {
+  for (var i = 0; i < elemArticle.length; i++) {
+    elemArticle[i].classList.add('opacityZeroAnimate');
+  }
+   setTimeout(returnArticle, 4000)
+}
+function returnArticle() {
+  for (var i = 0; i < elemArticle.length; i++) {
+  elemArticle[i].childNodes[1].classList.remove('opacityZeroAnimate');
+  elemArticle[i].childNodes[3].childNodes[3].classList.remove('opacityZeroAnimate');
+  elemArticle[i].childNodes[3].childNodes[1].classList.remove('opacityZeroAnimate');
+  elemArticle[i].classList.remove('opacityZeroAnimate');
+  }
+}
