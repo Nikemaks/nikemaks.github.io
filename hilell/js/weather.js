90bb15c8d176569bd7dba7bbac23c99a0a00c460
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-04-02 13:07:12 +0300

    weather

---
+++ hilell/js/weather.js
@@ -0,0 +1,31 @@
+function getWeather() {
+	let link = `https://api.openweathermap.org/data/2.5/weather?q=`,
+		api = 'APPID=4e98f7909caead442516fe23441b2605',
+		input = document.querySelector('#pac-input');
+	
+	input.addEventListener('change', (event)=>{
+		setTimeout(()=>{	
+			let city = event.target.value;
+		fetch(`${link}${city}&${api}&units=metric`)
+			.then(
+				response=>{
+					return response.json();
+				}
+			)
+			.then(
+				weather => {
+					// console.log(weather);
+					let weatherDiv = document.querySelector('.header-weather');
+					weatherDiv.innerHTML = weather.main.temp + '  °C'
+				}
+			)
+			.catch(
+				error => {
+					console.log(error);
+				}
+			)	
+		})		
+	}, 10)
+
+}	
+getWeather();
