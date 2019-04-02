3a5d2c8946382a26dabc2c557ee6f8cfc2173789
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-04-02 18:17:06 +0300

    fix

--- hilell/js/NewPromise.js
+++ hilell/js/NewPromise.js
@@ -19,13 +19,16 @@ let promise = new Promise(function(resolve, reject){
 		if ( curr < 0) {
 		    clearInterval(update);
 		  	deleteDisabled();
-			btnTwo.addEventListener('click', ()=> resolve());
-
+			btnTwo.addEventListener('click', ()=> resolve(true));
 		}
 			progressBar.value = curr--;
 		}, 10)
 	}
 });
+
+let promise2 = new Promise(function (resolve, reject) {
+	btnOne.addEventListener('click', ()=> resolve(false));
+})
 function deleteDisabled(){
   btnOne.removeAttribute('disabled');
   btnTwo.removeAttribute('disabled');
@@ -40,7 +43,8 @@ for (let i = 0; i < elemArticle.length; i++) {
 		)
 		.then(
 			result => {
-				let event = elemArticle[i].children[0].addEventListener('animationend', function addClass(){
+				elemArticle[i].children[0].addEventListener('animationend', function addClass(event){
+					event.stopPropagation();
 					elemArticle[i].children[1].children[1].classList.add('opacityZeroAnimate');
 				});
 				return promise;
@@ -49,28 +53,59 @@ for (let i = 0; i < elemArticle.length; i++) {
 		)
 		.then(
 			result => {
-				elemArticle[i].children[1].children[1].addEventListener('animationend', ()=>{
+				elemArticle[i].children[1].children[1].addEventListener('animationend', (event)=>{
+					event.stopPropagation();
 					elemArticle[i].children[1].children[0].classList.add('opacityZeroAnimate');
 					return promise;
 				})
 			}
-		).then(
+		)
+		.then(
 			result => {	
-				elemArticle[i].children[1].children[0].addEventListener('animationend', ()=>{
+				elemArticle[i].children[1].children[0].addEventListener('animationend', (event)=>{
+					event.stopPropagation();
 					elemArticle[i].classList.add('opacityZeroAnimate');
 					return promise;
 				})
 			}	
-		).then(
-			// elemArticle[i].addEventListener('animationend', ()=>{
-			//  returnAll(i)
-			// })
+		)
+		.then(
+			elemArticle[i].addEventListener('animationend', (event)=>{
+				event.stopPropagation();
+			 	returnAll();
+			})
 		);	
 }
-function returnAll(i) {
-	if (i === 2) {
+function returnAll() {
 		for (let i = 0; i < elemArticle.length; i++) {
+			event.stopPropagation();
 			elemArticle[i].classList.add('opacityZeroAnimateRev');
+			elemArticle[i].children[1].children[0].classList.add('opacityZeroAnimateRev');
+			elemArticle[i].children[1].children[1].classList.add('opacityZeroAnimateRev');
+			elemArticle[i].children[0].classList.add('opacityZeroAnimateRev');
 		}	
-	}
  }
+
+ for (let i = 0; i < elemArticle.length; i++) {
+	promise2
+	  .then(
+	  	result => {
+			 hideElem(i)
+	  		console.log('11');
+	  	}
+	  )
+	  .then(
+	
+	  )
+	  .then(
+
+	  )
+ }
+function hideElem(i){
+	event.stopPropagation();
+	elemArticle[i].classList.add('opacityZeroAnimate');
+	elemArticle[i].children[1].children[0].classList.add('opacityZeroAnimate');
+	elemArticle[i].children[1].children[1].classList.add('opacityZeroAnimate');
+	elemArticle[i].children[0].classList.add('opacityZeroAnimate');
+
+}
