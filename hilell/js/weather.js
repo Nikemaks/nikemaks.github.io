2dabf180954dae09e158f9657ad290a2919bc2f6
Максим <35004603+Nikemaks@users.noreply.github.com>
2019-04-03 17:40:03 +0300

    weather

--- hilell/js/weather.js
+++ hilell/js/weather.js
@@ -2,33 +2,50 @@ function getWeather() {
 	let link = `https://api.openweathermap.org/data/2.5/weather?q=`,
 		api = 'APPID=4e98f7909caead442516fe23441b2605',
 		input = document.querySelector('#pac-input');
-	
-	input.addEventListener('change', (event)=>{
-		setTimeout(()=>{	
-			let city = event.target.value;
-		fetch(`${link}${city}&${api}&units=metric`)
-			.then(
-				response=>{
-					document.querySelector('.header-weather').children[0].innerHTML = '';
-					return response.json();
-				}
-			)
-			.then(
-				weather => {
-					let weatherDiv = document.querySelector('.header-weather'),
-						img = document.createElement('img');
-						img.setAttribute('src',`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
-					weatherDiv.children[0].appendChild(img);
-					weatherDiv.children[1].innerHTML =  weather.main.temp + ' °C';
-				}
-			)
-			.catch(
-				error => {
-					console.log(error);
-				}
-			)	
-		})		
-	}, 10)
 
-}	
+		if (localStorage.getItem('city')) {
+			let city = localStorage.getItem('city');
+			input.value = localStorage.getItem('city');
+			console.log();
+			requestWeather(link, city, api);
+		}
+	input.addEventListener('change',getOptionWeather);
+
+		function getOptionWeather(event){
+			setTimeout((event)=>{	
+				let city = input.value.split(','),
+					link = `https://api.openweathermap.org/data/2.5/weather?q=`,
+					api = 'APPID=4e98f7909caead442516fe23441b2605';
+				localStorage.setItem('city', city[0]);
+				requestWeather(link,city[0], api)
+			}, 10)
+		}
+		function requestWeather(link,city, api){
+			fetch(`${link}${city}&${api}&units=metric`)
+				.then(
+					response=>{
+						document.querySelector('.header-weather').children[0].innerHTML = '';
+						return response.json();
+					}
+				)
+				.then(
+					weather => {
+						let weatherDiv = document.querySelector('.header-weather'),
+							img = document.createElement('img');
+							img.setAttribute('src',`https://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+						weatherDiv.children[0].appendChild(img);
+						weatherDiv.children[1].innerHTML =  weather.main.temp + ' °C';
+					}
+				)
+				.catch(
+					error => {
+						console.log(error);
+					}
+				)
+		}
+				
+
+
+  }
+	
 getWeather();
